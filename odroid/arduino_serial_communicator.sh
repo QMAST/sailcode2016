91c9d1136b8ca1ba4f1e43913bbbe0e04ec89eba
Odroid <sailboat@engsoc.queensu.ca>
2016-04-15 14:13:05 -0400

    Fixed bug in arduino_serial_communication.sh

--- odroid/arduino_serial_communicator.sh
+++ odroid/arduino_serial_communicator.sh
@@ -5,12 +5,16 @@ if ! [[ -c /dev/ttyACM0 ]]; then
 	exit 1
 fi
 
+checkRequest=$(printf "R\r\n")
+echo $checkRequest
+
 while true; do
 	cat /dev/ttyACM0 | while read request; do
 		echo $request
-		if [[ $request="Request" ]]; then
+		if [[ $request = $checkRequest ]]; 
+		then
 			cat /tmp/ball_position > /dev/ttyACM0
-	#		sleep 1
+			sleep 1
 			echo -n "Request responded: "
 			read request;
 			echo $request
--- odroid/ball_tracker_caller.sh
+++ odroid/ball_tracker_caller.sh
@@ -2,13 +2,13 @@
 
 while true; do
 	out=$(~/sailcode/BoatVision/main 2>/dev/null) #> /tmp/ball_position 2>/dev/null
-	if [ $out != "N" ]; then
+	if [[ $out != "N" ]]; then
 		echo "$out" > /tmp/ball_position
 		misses="0"
 	fi
 	#If the ball cannot be located 5 times in a row, update buffer to "ball not found" value
 	misses=$((misses + 1))
-	if [ $misses -eq 5 ]; then
+	if [[ $misses -eq 5 ]]; then
 		echo "$out" > /tmp/ball_position
 		misses="0"
 	fi
